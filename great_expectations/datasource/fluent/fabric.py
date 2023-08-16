3170603c5329e62eabb3c1f996ea10843b9f7e17
Gabriel Gore <gabriel59kg@gmail.com>
2023-08-16 15:06:57 -0400

    create add_asset methods

--- great_expectations/datasource/fluent/fabric.py
+++ great_expectations/datasource/fluent/fabric.py
@@ -1,18 +1,23 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, ClassVar, List, Literal, Type, Union
+from typing import TYPE_CHECKING, ClassVar, List, Literal, Optional, Type, Union
 
 import pydantic
-from typing_extensions import Annotated
+from typing_extensions import Annotated, TypeAlias
 
 from great_expectations.datasource.fluent.interfaces import (
-    # Batch,
     DataAsset,
     Datasource,
+    Sorter,
 )
 
 if TYPE_CHECKING:
+    from great_expectations.datasource.fluent.interfaces import (
+        BatchMetadata,
+    )
     from great_expectations.execution_engine import PandasExecutionEngine
+
+SortersDefinition: TypeAlias = List[Union[Sorter, str, dict]]
 
 
 class PowerBIDax(DataAsset):
@@ -34,7 +39,7 @@ class PowerBITable(DataAsset):
     """Microsoft PowerBI Table."""
 
     type: Literal["powerbi_table"] = "powerbi_table"
-    schema_: str = pydantic.Field(..., alias="schema")
+    schema_: Optional[str] = pydantic.Field(None, alias="schema")
     table_name: str
 
 
@@ -70,3 +75,88 @@ class FabricDatasource(Datasource):
         )
 
         return PandasExecutionEngine
+
+    def add_powerbi_dax_asset(  # noqa: PLR0913
+        self,
+        name: str,
+        query: str,
+        dataset: str,
+        workspace: str,
+        order_by: Optional[SortersDefinition] = None,
+        batch_metadata: Optional[BatchMetadata] = None,
+    ) -> PowerBIDax:
+        """Adds a PowerBIDax asset to this datasource.
+
+        Args:
+            name: The name of this asset.
+            TODO: other args
+            order_by: A list of Sorters or Sorter strings.
+            batch_metadata: BatchMetadata we want to associate with this DataAsset and all batches derived from it.
+
+        Returns:
+            The asset that is added to the datasource.
+        """
+        order_by_sorters: list[Sorter] = self.parse_order_by_sorters(order_by=order_by)
+        asset = PowerBIDax(
+            name=name,
+            query=query,
+            dataset=dataset,
+            workspace=workspace,
+            order_by=order_by_sorters,
+            batch_metadata=batch_metadata or {},
+        )
+        return self._add_asset(asset)
+
+    def add_powerbi_measure_asset(
+        self,
+        name: str,
+        order_by: Optional[SortersDefinition] = None,
+        batch_metadata: Optional[BatchMetadata] = None,
+    ) -> PowerBIMeasure:
+        """Adds a PowerBIMeasure asset to this datasource.
+
+        Args:
+            name: The name of this asset.
+            order_by: A list of Sorters or Sorter strings.
+            batch_metadata: BatchMetadata we want to associate with this DataAsset and all batches derived from it.
+
+        Returns:
+            The asset that is added to the datasource.
+        """
+        order_by_sorters: list[Sorter] = self.parse_order_by_sorters(order_by=order_by)
+        asset = PowerBIMeasure(
+            name=name,
+            order_by=order_by_sorters,
+            batch_metadata=batch_metadata or {},
+        )
+        return self._add_asset(asset)
+
+    def add_powerbi_table_asset(  # noqa: PLR0913
+        self,
+        name: str,
+        table_name: str = "",
+        schema: Optional[str] = None,
+        order_by: Optional[SortersDefinition] = None,
+        batch_metadata: Optional[BatchMetadata] = None,
+    ) -> PowerBITable:
+        """Adds a PowerBITable asset to this datasource.
+
+        Args:
+            name: The name of this table asset.
+            table_name: The table where the data resides.
+            schema: The schema that holds the table.
+            order_by: A list of Sorters or Sorter strings.
+            batch_metadata: BatchMetadata we want to associate with this DataAsset and all batches derived from it.
+
+        Returns:
+            The asset that is added to the datasource.
+        """
+        order_by_sorters: list[Sorter] = self.parse_order_by_sorters(order_by=order_by)
+        asset = PowerBITable(
+            name=name,
+            table_name=table_name,
+            schema=schema,
+            order_by=order_by_sorters,
+            batch_metadata=batch_metadata or {},
+        )
+        return self._add_asset(asset)
